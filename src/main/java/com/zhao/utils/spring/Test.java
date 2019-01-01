bd8d707dcd763d6fed4190c00c6573c7e8478937
zh155906@163.com <zh155906@163.com>
2019-01-01 12:01:47 +0800

    合并代码

--- src/main/java/com/zhao/utils/spring/Test.java
+++ src/main/java/com/zhao/utils/spring/Test.java
@@ -15,17 +15,23 @@ public class Test {
 /*            ApplicationContext applicationContext = BeanFactory*/
 
 //        ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-        GenericGroovyApplicationContext context = new GenericGroovyApplicationContext();
+        /*GenericGroovyApplicationContext context = new GenericGroovyApplicationContext();
         new XmlBeanDefinitionReader(context).loadBeanDefinitions("applicationContext.xml");
         context.refresh();
         Class1 class1 = context.getBean("haha",Class1.class);
         class1.test();
 
         Class2.Class3 class3 = context.getBean("Class3", Class2.Class3.class);
-        class3.test();
+        class3.test();*/
 
+        testClass();
 
+    }
 
+    public static void testClass() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Class1 class1 = context.getBean("class1",Class1.class);
+        class1.testClass2();
     }
 
 }
