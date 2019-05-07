73b33848f77ab1aeabc3f567a7488c4dea5a0fca
zh155906@163.com <zh155906@163.com>
2019-05-07 22:52:54 +0800

    合并冲突

--- src/main/java/com/zhao/utils/CherryPickDemo.java
+++ src/main/java/com/zhao/utils/CherryPickDemo.java
@@ -7,11 +7,6 @@ public class CherryPickDemo {
         System.out.println("A");
         System.out.println("B");
 
-        System.out.println("C");
-        System.out.println("D");
-        System.out.println("F");
-        System.out.println("E");
-        System.out.println("G");
     }
 
 }
